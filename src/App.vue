a59456ba8a153b48b47694710faaf19df8f00f79
Reunkedze <reunok2000@gmail.com>
2025-09-06 13:27:17 +0300

    Модификаторы V-MODEL

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="app">
-    <post-form
-        @create="createPost"
-    />
+    <h1>Страница с постами</h1>
+    <my-button
+      @click="showDialog"
+      style="margin: 15px 0;"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
     <post-list
         :posts="posts"
         @remove="removePost"
@@ -13,8 +22,10 @@
 <script>
 import PostList from "./components/PostList.vue";
 import PostForm from "./components/PostForm.vue";
+import MyButton from "@/components/UI/MyButton.vue";
 export default {
   components: {
+    MyButton,
     PostList, PostForm
   },
   data() {
@@ -23,15 +34,20 @@ export default {
         {id: 1, title: 'html', body: 'about html'},
         {id: 2, title: 'css', body: 'about css'},
         {id: 3, title: 'javaSctipt', body: 'about js'},
-      ]
+      ],
+      dialogVisible: false,
     }
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
     }
   }
 }
