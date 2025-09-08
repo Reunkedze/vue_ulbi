3164680edd9f7ba4eef4feb1010ac4b634aaae9a
Reunkedze <reunok2000@gmail.com>
2025-09-08 21:01:29 +0300

    Поиск постов. Фильтрация

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -15,12 +15,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 style="color: red;" v-else>
     Список постов пуст
@@ -28,5 +30,25 @@
 </template>
 
 <style scoped>
+
+  .post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+
+  .post-list-enter-active,
+  .post-list-leave-active {
+    transition: all 0.2s ease;
+  }
+
+  .post-list-enter-from,
+  .post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+  }
+
+  .post-list-move {
+    transition: transform 0.3s ease;
+  }
 
 </style>
